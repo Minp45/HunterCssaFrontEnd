c67095ef7316be64cd112d95b64c0cdbe4d71800
Mike <minp4545@gmail.com>
2023-06-07 15:09:24 -0400

    change switch to render

--- huntercssa/src/App.js
+++ huntercssa/src/App.js
@@ -1,19 +1,20 @@
 import './App.css';
 
 //Router
-import { Switch, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 //Components
 
 import {
   HomePageContainer,
-
 } from './components/containers';
 
 function App() {
   return (
-    <Switch>
-        <Route exact path="/" component={HomePageContainer} />
-    </Switch> 
+    <>
+      <Routes>
+        <Route index path="/" element={<HomePageContainer />} />
+      </Routes>
+    </>
   );
 }
 
