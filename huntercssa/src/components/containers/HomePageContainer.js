347b281dcf6a8275a6331ad1432560af6b11ad5c
Mike <minp4545@gmail.com>
2023-06-09 21:25:59 -0400

    finish animation sliding

--- huntercssa/src/components/containers/HomePageContainer.js
+++ huntercssa/src/components/containers/HomePageContainer.js
@@ -1,10 +1,12 @@
 import HomePage from '../views/HomePageView';
 import Footer from '../views/FooterView';
+import PosterSliding from '../views/PosterSlidingView';
 
 const HomePageContainer = () => {
   return (
     <>
       <HomePage />
+      <PosterSliding />
       <Footer />
     </>
   );
